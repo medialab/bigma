ed6fb6e19adbdff9be3400f988c034f3facb527c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2022-08-25 19:01:07 +0200

    finetune interface and add nodes search

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import fetchline from "fetchline";
 import {Sigma} from "sigma";
 import DirectedGraph from "graphology";
-import FA2Layout from "graphology-layout-forceatlas2/worker.js";
-import forceAtlas2 from "graphology-layout-forceatlas2";
 
 const graph = new DirectedGraph();
 
@@ -14,7 +12,6 @@ const loadPositions = async (positions) => {
   title.textContent = "Loading nodes…";
   orderSpan.textContent = "…";
   document.querySelectorAll("#sigma-container canvas").forEach(e => e.parentNode.removeChild(e))
-  document.getElementById("fa2").style.display = "none";
   document.getElementById('positions_file').disabled = "disabled";
 
   let order = 0;
@@ -24,7 +21,7 @@ const loadPositions = async (positions) => {
     try {
       graph.addNode(node, {x: parseFloat(xPos), y: parseFloat(yPos)});
       order++;
-      if (order % 5000 == 0)
+      if (order % 1000 == 0)
         orderSpan.textContent = order + "…";
     } catch(e) {
       console.log("ERROR loading node:", line, e);
@@ -33,20 +30,19 @@ const loadPositions = async (positions) => {
   orderSpan.textContent = order + "";
   document.getElementById("loadEdges").style.opacity = 1;
   if (graph.size) {
-    title.textContent = "Affecting nodes size…"
+    title.textContent = "Affecting nodes sizes…"
     setTimeout(renderGraph, 0);
   }
 }
 
 // Read edges
 const loadEdges = async (edgesfile) => {
-  console.log("Loading edges file:", edgesfile);
+  console.log("Loading edges file:", edgesfile.name);
   const sizeSpan = document.getElementById("size"),
     title = document.getElementById("title");
   title.textContent = "Loading edges…";
   sizeSpan.textContent = "…";
   document.querySelectorAll("#sigma-container canvas").forEach(e => e.parentNode.removeChild(e))
-  document.getElementById("fa2").style.display = "none";
   document.getElementById('edges_file').disabled = "disabled";
 
   let size = 0;
@@ -56,7 +52,7 @@ const loadEdges = async (edgesfile) => {
     try {
       graph.addEdge(source, target, {weight: parseInt(weight)});
       size++;
-      if (size % 50000 == 0)
+      if (size % 10000 == 0)
         sizeSpan.textContent = size + "…";
     } catch(e) {
       console.log("ERROR loading edge:", line, e);
@@ -73,7 +69,7 @@ const renderGraph = () => {
 // Adjust nodes sizes
   graph.forEachNode((node, { cluster }) => {
     graph.mergeNodeAttributes(node, {
-      size: Math.sqrt(graph.degree(node)) / 10,
+      size: Math.sqrt(graph.degree(node)) / 6,
       //color: colors[cluster + ""],
       label: node
     });
@@ -84,30 +80,78 @@ const renderGraph = () => {
     // Render the graph:
     const container = document.getElementById("sigma-container");
     const renderer = new Sigma(graph, container, {
-      defaultEdgeColor: "#efefef"
+      defaultEdgeColor: "#efefef",
+      minCameraRatio: 0.1,
+      maxCameraRatio: 10
+    });
+    document.getElementById("controls").style.display = "block";
+
+    // Enable Zoombuttons:
+    const camera = renderer.getCamera();
+    document.getElementById("zoom-in").addEventListener("click", () => {
+      camera.animatedZoom({ duration: 600 });
+    });
+    document.getElementById("zoom-out").addEventListener("click", () => {
+      camera.animatedUnzoom({ duration: 600 });
+    });
+    document.getElementById("zoom-reset").addEventListener("click", () => {
+      camera.animatedReset({ duration: 600 });
     });
 
-    // Enable FA2 button:
-    document.getElementById("fa2").style.display = "block";
-    const fa2Button = document.getElementById("fa2");
-    const sensibleSettings = forceAtlas2.inferSettings(graph);
-    const fa2Layout = new FA2Layout(graph, {
-      settings: sensibleSettings,
+    // Bind labels threshold to range input
+    const labelsThresholdRange = document.getElementById("labels-threshold");
+    labelsThresholdRange.addEventListener("input", () => {
+      renderer.setSetting("labelRenderedSizeThreshold", -labelsThresholdRange.value);
     });
-    function toggleFA2Layout() {
-      if (fa2Layout.isRunning()) {
-        fa2Layout.stop();
-        fa2Button.innerHTML = `Start layout ▶`;
-      } else {
-        fa2Layout.start();
-        fa2Button.innerHTML = `Stop layout ⏸`;
+    labelsThresholdRange.value = -renderer.getSetting("labelRenderedSizeThreshold") + "";
+
+    // Setup nodes search
+    const searchInput = document.getElementById("search-input");
+    const searchSuggestions = document.getElementById("suggestions");
+    let selectedNode = null,
+      suggestions = [];
+
+    const setSearchQuery = (query) => {
+      if (searchInput.value !== query) searchInput.value = query;
+      if (query.length > 1) {
+        const lcQuery = query.toLowerCase();
+        suggestions = [];
+        graph.forEachNode((node) => {
+          if (node.toLowerCase().includes(lcQuery))
+            suggestions.push(node);
+        });
+
+        if (suggestions.length === 1 && suggestions[0] === query) {
+          if (selectedNode)
+            graph.setNodeAttribute(selectedNode, "highlighted", false);
+          selectedNode = suggestions[0];
+          suggestions = [];
+          graph.setNodeAttribute(selectedNode, "highlighted", true);
+          let view = renderer.getNodeDisplayData(selectedNode);
+          view.ratio = camera.ratio / 1.5;
+          camera.animate(view, {duration: 1000});
+        } else if (selectedNode) {
+          graph.setNodeAttribute(selectedNode, "highlighted", false);
+          selectedNode = null;
+        }
+      } else if (selectedNode) {
+        graph.setNodeAttribute(selectedNode, "highlighted", false);
+        selectedNode = null;
+        suggestions = [];
       }
+      searchSuggestions.innerHTML = suggestions
+        .sort()
+        .map((node) => "<option value=" + node + "></option>")
+        .join("\n");
+      // Refresh rendering:
+      renderer.refresh();
     }
-    fa2Button.addEventListener("click", toggleFA2Layout);
 
-    // Cheap trick: tilt the camera a bit to make labels more readable:
-    renderer.getCamera().setState({
-      angle: 0.2,
+    searchInput.addEventListener("input", () => {
+      setSearchQuery(searchInput.value || "");
+    });
+    searchInput.addEventListener("blur", () => {
+      setSearchQuery("");
     });
 
     title.textContent = "Graph ready";
