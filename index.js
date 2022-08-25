e69d881cbbc248385b684b16db519df5b014a184
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2022-08-25 15:23:17 +0200

    fix loading files one by one

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ import DirectedGraph from "graphology";
 import FA2Layout from "graphology-layout-forceatlas2/worker.js";
 import forceAtlas2 from "graphology-layout-forceatlas2";
 
-const loadGraph = async (positions) => {
-  const graph = new DirectedGraph(),
-    orderSpan = document.getElementById("order"),
-    sizeSpan = document.getElementById("size"),
-    title = document.getElementById("title"),
-    edgesfile = positions.replace(/_positions_after.*$/, '');
-  title.textContent = "Loading graph…";
-  orderSpan.textContent = "…";
-  sizeSpan.textContent = "…";
-  document.querySelectorAll("#sigma-container canvas").forEach(e => e.parentNode.removeChild(e))
-  document.getElementById("fa2").style.display = "none";
+const graph = new DirectedGraph();
 
 // Read nodes positions:
-  let order = 0, size = 0;
-  for await (const line of fetchline(positions)) {
+const loadPositions = async (positions) => {
+  console.log("Loading positions file:", positions.name);
+  const orderSpan = document.getElementById("order"),
+    title = document.getElementById("title");
+  title.textContent = "Loading nodes…";
+  orderSpan.textContent = "…";
+  document.querySelectorAll("#sigma-container canvas").forEach(e => e.parentNode.removeChild(e))
+  document.getElementById("fa2").style.display = "none";
+  document.getElementById('positions_file').disabled = "disabled";
+
+  let order = 0;
+  for await (const line of fetchline(URL.createObjectURL(positions))) {
     const [node, xPos, yPos] = line.split(/,/);
-    if (!node || node === "Node") continue;
+    if (!node || node.toLowerCase() === "node") continue;
     try {
       graph.addNode(node, {x: parseFloat(xPos), y: parseFloat(yPos)});
       order++;
@@ -31,12 +31,28 @@ const loadGraph = async (positions) => {
     }
   }
   orderSpan.textContent = order + "";
-  sizeSpan.textContent = "…";
+  document.getElementById("loadEdges").style.opacity = 1;
+  if (graph.size) {
+    title.textContent = "Affecting nodes size…"
+    setTimeout(renderGraph, 0);
+  }
+}
 
 // Read edges
-  for await (const line of fetchline(edgesfile)) {
+const loadEdges = async (edgesfile) => {
+  console.log("Loading edges file:", edgesfile);
+  const sizeSpan = document.getElementById("size"),
+    title = document.getElementById("title");
+  title.textContent = "Loading edges…";
+  sizeSpan.textContent = "…";
+  document.querySelectorAll("#sigma-container canvas").forEach(e => e.parentNode.removeChild(e))
+  document.getElementById("fa2").style.display = "none";
+  document.getElementById('edges_file').disabled = "disabled";
+
+  let size = 0;
+  for await (const line of fetchline(URL.createObjectURL(edgesfile))) {
     const [source, target, weight] = line.split(/,/);
-    if (!source || source === "Source") continue;
+    if (!source || source.toLowerCase() === "source") continue;
     try {
       graph.addEdge(source, target, {weight: parseInt(weight)});
       size++;
@@ -47,55 +63,60 @@ const loadGraph = async (positions) => {
     }
   }
   sizeSpan.textContent = size + "";
-  title.textContent = "Affecting nodes size…"
+  if (graph.order) {
+    title.textContent = "Affecting nodes size…"
+    setTimeout(renderGraph, 0);
+  }
+}
+
+const renderGraph = () => {
+// Adjust nodes sizes
+  graph.forEachNode((node, { cluster }) => {
+    graph.mergeNodeAttributes(node, {
+      size: Math.sqrt(graph.degree(node)) / 10,
+      //color: colors[cluster + ""],
+      label: node
+    });
+  });
+  title.textContent = "Rendering graph…";
 
   setTimeout(() => {
-// Adjust nodes sizes
-    graph.forEachNode((node, { cluster }) => {
-      graph.mergeNodeAttributes(node, {
-        size: Math.sqrt(graph.degree(node)) / 10,
-        //color: colors[cluster + ""],
-        label: node
-      });
+    // Render the graph:
+    const container = document.getElementById("sigma-container");
+    const renderer = new Sigma(graph, container, {
+      defaultEdgeColor: "#efefef"
     });
-    title.textContent = "Rendering graph…";
 
-    setTimeout(() => {
-      // Render the graph:
-      const container = document.getElementById("sigma-container");
-      const renderer = new Sigma(graph, container, {
-        defaultEdgeColor: "#efefef"
-      });
-
-      // Enable FA2 button:
-      document.getElementById("fa2").style.display = "block";
-      const fa2Button = document.getElementById("fa2");
-      const sensibleSettings = forceAtlas2.inferSettings(graph);
-      const fa2Layout = new FA2Layout(graph, {
-        settings: sensibleSettings,
-      });
-      function toggleFA2Layout() {
-        if (fa2Layout.isRunning()) {
-          fa2Layout.stop();
-          fa2Button.innerHTML = `Start layout ▶`;
-        } else {
-          fa2Layout.start();
-          fa2Button.innerHTML = `Stop layout ⏸`;
-        }
+    // Enable FA2 button:
+    document.getElementById("fa2").style.display = "block";
+    const fa2Button = document.getElementById("fa2");
+    const sensibleSettings = forceAtlas2.inferSettings(graph);
+    const fa2Layout = new FA2Layout(graph, {
+      settings: sensibleSettings,
+    });
+    function toggleFA2Layout() {
+      if (fa2Layout.isRunning()) {
+        fa2Layout.stop();
+        fa2Button.innerHTML = `Start layout ▶`;
+      } else {
+        fa2Layout.start();
+        fa2Button.innerHTML = `Stop layout ⏸`;
       }
-      fa2Button.addEventListener("click", toggleFA2Layout);
+    }
+    fa2Button.addEventListener("click", toggleFA2Layout);
 
-      // Cheap trick: tilt the camera a bit to make labels more readable:
-      renderer.getCamera().setState({
-        angle: 0.2,
-      });
+    // Cheap trick: tilt the camera a bit to make labels more readable:
+    renderer.getCamera().setState({
+      angle: 0.2,
+    });
 
-      title.textContent = "Graph ready";
-    }, 0);
+    title.textContent = "Graph ready";
   }, 0);
-  return;
 }
 
 document.getElementById('positions_file').addEventListener('change', (event) => {
-  loadGraph(event.target.files[0].name);
+  loadPositions(event.target.files[0]);
+});
+document.getElementById('edges_file').addEventListener('change', (event) => {
+  loadEdges(event.target.files[0]);
 });
